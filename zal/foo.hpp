ef27c544d5b90252280b19103aeba05bb899d6f8
Gabriela Machalska <gabriela.machalska@gmail.com>
2020-12-16 14:05:57 +0000

    change

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -10,7 +10,8 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
 
-    // Twoja implementacja tutaj
-
+auto it = ret_v.end();
+std::for_each(people.begin(), people.end(),
+[&it](Human & h) { h.birthday(); *(--it) = (h.isMonster() ? 'n' : 'y'); });
     return ret_v;
 }
